b0f2b68ac87dec1542d0e2c604c432d4e04bdec0
wujianhui <woojh0218@gmail.com>
2020-01-09 20:36:39 +0800

    tmp

---
+++ leetcode/al23.go
@@ -0,0 +1,23 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	// headSort lists
+	// pop min, add the next of the popped into the array
+	//
+}
+
+func heapSort(lists []*ListNode) {
+
+}
+
+func replaceMin(lists []*ListNode, size int) {
+
+}
